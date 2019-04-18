bb855b768001bfe997d8a36790f8176c894b1916
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-04-18 16:30:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "241049f449c818a1636431d8d348ef7d9f0590f777c5dbe5c9f5552996eb154e"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    #do stuff with session here
+    erb :checkout
+  end
+  
 end
